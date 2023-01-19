8592343e221233e6d150aa62b01e662ee59a9e36
Pik <pikzaa@gmail.com>
2023-01-19 16:49:48 +0700

    add: redirect meta tag

--- src/app/components/redirect/redirect.component.ts
+++ src/app/components/redirect/redirect.component.ts
@@ -14,16 +14,45 @@ export class RedirectComponent implements OnInit {
   ngOnInit(): void {
     if (window.location.pathname.includes('/abc')) {
       this.msg = 'redirect..';
-      window.location.href =
-        'https://storage.googleapis.com/paotang-static-file/DEEPLINK/paotang-external-deeplink.html?openExternalBrowser=1&destination=missionWeb';
+      this.meta.addTag({
+        httpEquiv: 'refresh',
+        content:
+          '0; url=https://storage.googleapis.com/paotang-static-file/DEEPLINK/paotang-external-deeplink.html?openExternalBrowser=1&destination=missionWeb',
+      });
     } else if (window.location.pathname.includes('/xyz')) {
       this.msg = 'redirect..';
-      window.location.href =
-        'https://storage.googleapis.com/paotang-static-file/DEEPLINK/paotang-external-deeplink.html?openExternalBrowser=1&destination=missionWeb&page=tracking&campaignCode=00000000-0000-0000-0000-0000000ca003';
+      this.meta.addTag({
+        httpEquiv: 'refresh',
+        content:
+          '2; url=https://storage.googleapis.com/paotang-static-file/DEEPLINK/paotang-external-deeplink.html?openExternalBrowser=1&destination=missionWeb&page=tracking&campaignCode=00000000-0000-0000-0000-0000000ca003',
+      });
     } else if (window.location.pathname.includes('/testing')) {
       this.msg = 'redirect..';
-      window.location.href =
-        'https://bitakou.github.io/test-pdf-viewer-ngx/';
+      window.location.href = 'https://bitakou.github.io/test-pdf-viewer-ngx/';
+    } else if (window.location.pathname.includes('/nothing')) {
+      this.msg = 'nothing here';
+      this.meta.updateTag({
+        property: 'title',
+        content: 'title: Nothing change in this page',
+      });
+      this.meta.updateTag({
+        property: 'description',
+        content: 'description: test description here!',
+      });
+      this.meta.updateTag({
+        property: 'og:title',
+        content: 'og:title Nothing change in this page',
+      });
+      this.meta.updateTag({ property: 'og:type', content: 'website' });
+      this.meta.updateTag({
+        property: 'og:url',
+        content: 'https://nebtry-new.github.io/image-share/nothing',
+      });
+      this.meta.updateTag({
+        property: 'og:image',
+        content:
+          'https://mission-static-file-sit.storage.googleapis.com/mock/campaign-spending-value.png',
+      });
     } else {
       this.msg = 'invalid url';
     }
